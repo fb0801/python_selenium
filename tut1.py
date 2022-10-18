863ff142d9037b139c522fedc732a1518a7ff162
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-10-18 21:37:19 +0100

    Add files via upload

--- tut1.py
+++ tut1.py
@@ -2,6 +2,7 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from main import *
 
 #https://github.com/mozilla/geckodriver/releases
@@ -12,9 +13,11 @@ browser.get("https://lsbuuniversity.powerhousehub.net/")
 time.sleep(3)
 
 #user= browser.find_element_by_name("username")
-element = browser.find_element_by_id("username")
+#element =browser.find_element(By.ID, ‘username’)
+element =browser.find_element(By.NAME, "username")
+#element = browser.find_element_by_id("username")
 
-element_2 = browser.find_element_by_id("password")
+element_2 = browser.find_element(By.ID,"password")
 #pwd = browser.find_element_by_name("password")
 
 element.send_keys("test")
