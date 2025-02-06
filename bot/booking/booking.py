67d406bb6f2d745236cc9b48f98e586fd07d1eab
fb0801 <fbhatti0801@gmail.com>
2025-02-06 18:00:11 +0000

    deal searching 2 and more

--- bot/booking/booking.py
+++ bot/booking/booking.py
@@ -1,28 +1,30 @@
 from selenium import webdriver
 import booking.constants as const
 import os
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 
-class Booking(webdriver.Firefox):
+class Booking:
     def __init__(self, teardown=False):
-        #self.browser = webdriver.Chrome
-        #self.driver_path = driver_path
+        
+        self.driver = webdriver.Firefox()
         self.teardown = teardown
-        super(Booking, self).__init__()
-        #os.environ['PATH'] += self.driver_path
+        
        
-        self.implicitly_wait(15)
-        self.maximize_window()
+        self.driver.implicitly_wait(15)  # Apply implicit wait
+        self.driver.maximize_window()  # Maximize browser window
 
     def __exit__(self, exc_type, exc_val, excel_tab):
         if self.teardown:
-            self.quit()
+            self.driver.quit()
 
     def land_first_page(self):
-        self.get(const.BASE_URL)
+        self.driver.get(const.BASE_URL)
 
-    def change_currency(self, ):
-        pass
+    def change_currency(self, currency=None):
+        currency_element = self.driver.find_element(By.CSS_SELECTOR, 
+                'button[data-tooltip-text="Choose your currency"]'
+        )
+        currency_element.click()
         
-
-
