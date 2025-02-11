67fd95851dde5e44368862b96c9ff2bf3f55b6be
fb0801 <fbhatti0801@gmail.com>
2025-02-11 18:00:41 +0000

    deal searching 2.2 and more

--- bot/booking/booking.py
+++ bot/booking/booking.py
@@ -8,26 +8,36 @@ from selenium.webdriver.common.by import By
 class Booking:
     def __init__(self, teardown=False):
         
-        self.driver = webdriver.Firefox()
         self.teardown = teardown
-        
-       
+        self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(15)  # Apply implicit wait
         self.driver.maximize_window()  # Maximize browser window
 
-    def __exit__(self, exc_type, exc_val, excel_tab):
+    
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Ensures the browser closes when exiting the context.
+        """
         if self.teardown:
             self.driver.quit()
 
     def land_first_page(self):
-        self.driver.get(const.BASE_URL)
+        """
+        Opens the base URL.
+        """
+        self.driver.get(const.BASE_URL)  # Use self.driver to call get()
 
     def change_currency(self, currency=None):
         currency_element = self.driver.find_element(By.CSS_SELECTOR, 
                 'button[data-tooltip-text="Choose your currency"]'
         )
         currency_element.click()
-        selected_currency_element = self.find_element(By.CSS_SELECTOR,
-            'a[data-modal-header-async-url-param*="selected_currency=USD"]'                                              
+        selected_currency_element = self.driver.find_element(By.CSS_SELECTOR,
+            f'a[data-modal-header-async-url-param*="selected_currency={currency}"]'                                              
         )
-        
+        selected_currency_element.click()
+
+    def select_place_to_go(self, place_to_go):
+        search_feild = self.driver.find_element(By.ID, 'ss')
+        search_feild.clear()
--- bot/run.py
+++ bot/run.py
@@ -3,5 +3,9 @@ from booking.booking import Booking
 
 with Booking() as bot:
     bot.land_first_page()
-    bot.change_currency("")
+    bot.change_currency(currency='GBP')
      
+
+
+
+
