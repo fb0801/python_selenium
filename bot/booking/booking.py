e5b09985bb252807b9432434bc433f80ec2cb8aa
fb0801 <fbhatti0801@gmail.com>
2025-02-12 17:37:23 +0000

    deal searching 2.4 selecting dates

--- bot/booking/booking.py
+++ bot/booking/booking.py
@@ -9,9 +9,9 @@ class Booking:
     def __init__(self, teardown=False):
         
         self.teardown = teardown
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(15)  # Apply implicit wait
-        self.driver.maximize_window()  # Maximize browser window
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(15)  # Apply implicit wait
+        self.browser.maximize_window()  # Maximize browser window
 
     
 
@@ -20,25 +20,41 @@ class Booking:
         Ensures the browser closes when exiting the context.
         """
         if self.teardown:
-            self.driver.quit()
+            self.browser.quit()
 
     def land_first_page(self):
         """
         Opens the base URL.
         """
-        self.driver.get(const.BASE_URL)  # Use self.driver to call get()
+        self.browser.get(const.BASE_URL)  # Use self.driver to call get()
 
     def change_currency(self, currency=None):
-        currency_element = self.driver.find_element(By.CSS_SELECTOR, 
+        currency_element = self.browser.find_element(By.CSS_SELECTOR, 
                 'button[data-tooltip-text="Choose your currency"]'
         )
         currency_element.click()
-        selected_currency_element = self.driver.find_element(By.CSS_SELECTOR,
+        selected_currency_element = self.browser.find_element(By.CSS_SELECTOR,
             f'a[data-modal-header-async-url-param*="selected_currency={currency}"]'                                              
         )
         selected_currency_element.click()
 
     def select_place_to_go(self, place_to_go):
-        search_feild = self.driver.find_element(By.ID, 'ss')
+        search_feild = self.browser.find_element(By.ID, 'ss')
         search_feild.clear()
         search_feild.send_keys(place_to_go)
+
+        first_result = self.browser.find_element(By.CSS_SELECTOR, 
+                'li[data-i="0"]'
+        )
+        first_result.click()
+
+    def select_dates(self, check_in_date, check_out_date):
+        check_in_element = self.browser.find_element(By.CSS_SELECTOR, 
+            f'td[data-date="{check_in_date}"]'
+        )
+        check_in_element.click()
+
+        check_out_element = self.browser.find_element(By.CSS_SELECTOR, 
+               f'td[data-date="{check_out_date}"]'
+        )
+        check_out_element.click()
--- bot/run.py
+++ bot/run.py
@@ -5,6 +5,8 @@ with Booking() as bot:
     bot.land_first_page()
     bot.change_currency(currency='GBP')
     bot.select_place_to_go('New York')
+    bot.select_dates(check_in_date="2025-05-16",
+                     check_out_date='2025-05-23')
      
 
 
