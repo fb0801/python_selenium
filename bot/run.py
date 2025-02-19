c3227e1bbbe8813dac3df2b38b2688e5f1ac5ae1
fb0801 <fbhatti0801@gmail.com>
2025-02-19 17:55:44 +0000

    execute from cli

--- bot/run.py
+++ bot/run.py
@@ -1,17 +1,16 @@
 from booking.booking import Booking
 
 
-with Booking() as bot:
-    bot.land_first_page()
-    bot.change_currency(currency='GBP')
-    bot.select_place_to_go('New York')
-    bot.select_dates(check_in_date="2025-05-16",
-                     check_out_date='2025-05-23')
-    bot.select_adults(1)
-    bot.click_search()
-    bot.apply_filtrations()
-     
+try:
+    with Booking() as bot:
+        bot.land_first_page()
+        bot.change_currency(currency='GBP')
+        bot.select_place_to_go('New York')
+        bot.select_dates(check_in_date="2025-05-16",
+                        check_out_date='2025-05-23')
+        bot.select_adults(1)
+        bot.click_search()
+        bot.apply_filtrations()
 
-
-
-
+except Exception as e:
+    print("There was a issue running this application")
