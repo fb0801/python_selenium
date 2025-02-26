38aae13812412e5ab6f4eb7f9929fd16786b174e
fb0801 <fbhatti0801@gmail.com>
2025-02-26 17:58:30 +0000

    deal reporting 1.4.3.1 user input

--- bot/run.py
+++ bot/run.py
@@ -8,7 +8,7 @@ try:
         bot.select_place_to_go(input('Where do you want to go?: '))
         bot.select_dates(check_in_date=input("What is the check in date"),
                         check_out_date=input('What is the check out date: '))
-        bot.select_adults(input("Number of people: "))
+        bot.select_adults(int(input("Number of people: ")))
         bot.click_search()
         bot.apply_filtrations()
         bot.refresh()
