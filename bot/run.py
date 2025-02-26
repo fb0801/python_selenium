5283f703ef326554c0427bf6b8d8ca61f84e628b
fb0801 <fbhatti0801@gmail.com>
2025-02-26 17:55:55 +0000

    deal reporting 1.4.3 user input

--- bot/run.py
+++ bot/run.py
@@ -5,10 +5,10 @@ try:
     with Booking() as bot:
         bot.land_first_page()
         bot.change_currency(currency='GBP')
-        bot.select_place_to_go('New York')
-        bot.select_dates(check_in_date="2025-05-16",
-                        check_out_date='2025-05-23')
-        bot.select_adults(1)
+        bot.select_place_to_go(input('Where do you want to go?: '))
+        bot.select_dates(check_in_date=input("What is the check in date"),
+                        check_out_date=input('What is the check out date: '))
+        bot.select_adults(input("Number of people: "))
         bot.click_search()
         bot.apply_filtrations()
         bot.refresh()
